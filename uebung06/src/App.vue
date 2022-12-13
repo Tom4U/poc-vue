b8546ceeefb46dc9a2dc8d130b03841d323ead87
Tom Ohms <tom@ohms.pro>
2022-12-13 10:17:02 +0100

    loesung06

---
+++ uebung06/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="container px-6 py-16 prose">
+    <div id="tasks-header">
+      <h1 class="text-center">{{ title }}</h1>
+      <button class="btn btn-primary" v-bind:disabled="!canAddTask">Aufgabe hinzufügen</button>
+      <hr>
+    </div>
+    <div id="tasks-list">
+      <section>
+        <section id="no-tasks" v-bind:class="{hidden: tasks.length > 0}">Keine Aufgaben verfügbar</section>
+      </section>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'Aufgabenliste',
+      canAddTask: true,
+      tasks: []
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hidden {
+  display: none;
+}
+</style>
